cbbb24f597dd52e26e919bf2e2db275fae80b5b3
Godfrey Karugu <karugugk@gmail.com>
2023-10-02 23:05:01 +0300

    Completed strings-review lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+
+const currentUser = " Alex James";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` 
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK,  ${upperCaseCurrentUser}!`;
+
+const shortGreeting =`Welcome, ${currentUser.slice(0,1)}!`
